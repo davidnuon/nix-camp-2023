6f007dfd256d04a17e863ab2719f25d31e4f2192
David Nuon <davidnuongm@gmail.com>
2023-09-30 15:57:27 +0100

    Add flake support

--- flake.nix
+++ flake.nix
@@ -4,14 +4,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
     nix2html-src.url = "github:davidnuon/nix2html/v0.0.3";
-    revealJS-src.url = "github:hakimel/reveal.js/v4.6.0";
-    slides = {
-      self,
-      nix2html-src,
-    }:
-      import ./slides {
-        inherit nix2html-src;
-      };
+    nix2html-src.flake = false;
+    revealJS-src.url = "github:hakimel/reveal.js";
+    revealJS-src.flake = false;
   };
 
   outputs = {
@@ -19,10 +14,36 @@
     nixpkgs,
     nix2html-src,
     revealJS-src,
-    slides,
-  }: {
-    defaultPackage =
-      nixpkgs.stdenv.mkDerivation rec {
-      };
+  }: let
+    nix2html = import nix2html-src {};
+    revealJS = revealJS-src;
+
+    slides = import ./slides {inherit nix2html;};
+    fileContents = nix2html.render slides.slides;
+  in {
+    inherit fileContents;
+    inherit revealJS;
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs {system = "x86_64-linux";};
+        stdenv.mkDerivation {
+          inherit revealJS fileContents;
+          outputFile = writeText "index.html" self.fileContents;
+
+          name = "nix-camp-slides";
+          src = self;
+
+          buildPhase = '''';
+          installPhase = ''
+            mkdir $out
+            cp -r $revealJS/plugin $out
+            cp $revealJS/dist/reset.css $out
+            cp $revealJS/dist/reveal.css $out
+            cp $revealJS/dist/reveal.js $out
+            cp -R $revealJS/dist/theme $out/theme
+            cp $outputFile $out/index.html
+          '';
+        };
   };
 }
